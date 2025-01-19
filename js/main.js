a1f900c12ca4c6094ad332745b944dbee38027b7
Michele <micheledatii@gmail.com>
2025-01-19 21:35:50 +0800

    Update

--- js/main.js
+++ js/main.js
@@ -160,6 +160,21 @@
 		}
 	});
 
+	$('.blog-bg').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
 	$("#download-cv").tooltip({
 	});
 	
